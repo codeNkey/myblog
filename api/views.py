12a4d7d6e05e19913b89dd38fa9b6814299e3460
heejin2dat@naver.com <heejin2dat@naver.com>
2024-11-05 15:24:53 +0900

    test폴더에 첫시험

--- api/views.py
+++ api/views.py
@@ -7,4 +7,5 @@ class ApiPostLV(BaseListView):
     qs = context['object_list']
     postList = [ obj_to_post(obj, False) for obj in qs]
     return JsonResponse(data=postList, safe=False, status=200)
-                                  # 안전검사를 건너 뛰어라 안전한 데이터다, 강제적으로 상태를 200으로 고정 
+                                  # 안전검사를 건너 뛰어라 안전한 데이터다, 강제적으로 상태를 200으로 고정 하세요
+                                  
